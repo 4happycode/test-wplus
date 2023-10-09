48f79ac560fd668c24d3c7a148d7be317c9a06c0
Feny Erie Emawati <fenevnscnc@gmail.com>
2023-10-09 13:52:23 +0700

    First commit

---
+++ __init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import render_template, request, jsonify, render_template
+import requests
+
+app = Flask(__name__)
+
+# Kunci API IQAir Anda
+API_KEY = 'de4931a4-f8d6-4e16-a17d-c03a2ebf4c51'
+
+
+@app.route('/get_air_quality', methods=['GET'])
+def get_air_quality():
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+
+    # Lakukan permintaan ke API IQAir dengan menggunakan kunci API Anda
+    url = f'http://api.airvisual.com/v2/nearest_city?lat={latitude}&lon={longitude}&key={API_KEY}'
+    print('+_+_+_+')
+    print(url)
+    print('+_+_+_+')
+    try:
+        print('try')
+        response = requests.get(url)
+        if response.status_code == 200:
+            air_quality_data = response.json()
+            return jsonify(air_quality_data)
+        else:
+            return jsonify({'error': 'Gagal mengambil data kualitas udara dari IQAir API'}), 500
+    except Exception as e:
+        return jsonify({'error': 'Terjadi kesalahan saat mengambil data kualitas udara: ' + str(e)}), 500
+
+
+# @app.route('/')
+# def hello_world():
+#     return render_template('hello.html', data=API_KEY)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("dashboard.html")
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
